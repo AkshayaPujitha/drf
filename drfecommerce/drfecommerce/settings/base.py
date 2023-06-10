6aff16720bb82fec7efe031de926b8216948b6b8
pujitha <akshayapujithakolli@gmail.com>
2023-06-10 13:05:58 +0530

    settings updated

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     #External Packages
     "rest_framework",
-    #Internal Packages
+    #Internal Apps
+    "drfecommerce.product.apps.ProductConfig",
 ]
 
 MIDDLEWARE = [
